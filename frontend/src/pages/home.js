384a869dbbfccb21928d54c7a1a8de3a2c5b309d
valery-ish <valery-ish@yandex.ru>
2022-11-27 12:52:48 +0300

    port

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -5,7 +5,7 @@ const HomePage = () => {
   return (    
     <div className={styles.startMain}>
       <h1 className={styles.startTitle}>Get job fast!</h1>
-      <a href={`http://${process.env.REACT_APP_HOST}:3000/auth`} className={styles.startBtnLink}>
+      <a href={`http://${process.env.REACT_APP_HOST}:4000/auth`} className={styles.startBtnLink}>
         <div className={styles.startBtn}>Войти с hh.ru</div>
       </a>
     </div>
