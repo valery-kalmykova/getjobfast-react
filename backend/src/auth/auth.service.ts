977d76182fe9ce7e0f4374e834819c52500e9fe4
Valery Kalmykova <valery-ish@yandex.ru>
2023-03-23 21:53:58 +0300

    fix back

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@nestjs/common';
-import { UsersService } from '../users/users.service';
+// import { UsersService } from '../users/users.service';
 import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
   constructor(
-    private usersService: UsersService,
+    // private usersService: UsersService,
     private jwtService: JwtService,
   ) {}
 
--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,15 +1,15 @@
-import { Controller, Get, Request, UseGuards } from '@nestjs/common';
-// import { User } from './user.entity';
-import { UsersService } from './users.service';
-import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+// import { Controller, Get, Request, UseGuards } from '@nestjs/common';
+// // import { User } from './user.entity';
+// import { UsersService } from './users.service';
+// import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 
-@Controller('users')
-export class UsersController {
-  constructor(private usersService: UsersService) {}
+// @Controller('users')
+// export class UsersController {
+//   constructor(private usersService: UsersService) {}
 
-  // @UseGuards(JwtAuthGuard)
-  // @Get('/me')
-  // findMe(@Request() req: any): Promise<User> {
-  //   return this.usersService.findOne(req.user.userId);
-  // }
-}
+//   // @UseGuards(JwtAuthGuard)
+//   // @Get('/me')
+//   // findMe(@Request() req: any): Promise<User> {
+//   //   return this.usersService.findOne(req.user.userId);
+//   // }
+// }
